83b628815aeee601d84762db0f7d9a879d082fbe
Andrie Detera <202311439@fit.edu.ph>
2024-10-27 22:53:46 +0800

    carousel-mobile-fix

--- carousel.js
+++ carousel.js
@@ -31,5 +31,34 @@ if (window.matchMedia("(min-width: 768px)").matches) {
     }
   });
 } else {
-  $(multipleCardCarousel).addClass("slide");
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+
+  var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  var cardWidth = $(".carousel-item").width();
+
+  var scrollPosition = 0;
+  $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 0);
+
+  $("#carouselExampleControls .carousel-control-next").on("click", function () {
+    // Adjusted condition to allow scrolling to the last card
+    if (scrollPosition < carouselWidth - cardWidth) {
+      scrollPosition += cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+
+  $("#carouselExampleControls .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselExampleControls .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
 }
