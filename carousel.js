13b197012b37493aa962c1b5da40e02a51422550
Andrie Detera <202311439@fit.edu.ph>
2024-10-27 22:15:10 +0800

    pre-publish version

--- carousel.js
+++ carousel.js
@@ -8,11 +8,9 @@ if (window.matchMedia("(min-width: 768px)").matches) {
   var carouselWidth = $(".carousel-inner")[0].scrollWidth;
   var cardWidth = $(".carousel-item").width();
 
-  // Set scroll position to center on the 3rd card
-  var scrollPosition = 0.75 * cardWidth;
+  var scrollPosition = 0.9 * cardWidth;
   $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 0);
 
-  // Event listener for the 'next' button
   $("#carouselExampleControls .carousel-control-next").on("click", function () {
     if (scrollPosition < carouselWidth - cardWidth * 4) {
       scrollPosition += cardWidth;
@@ -23,7 +21,6 @@ if (window.matchMedia("(min-width: 768px)").matches) {
     }
   });
 
-  // Event listener for the 'prev' button
   $("#carouselExampleControls .carousel-control-prev").on("click", function () {
     if (scrollPosition > 0) {
       scrollPosition -= cardWidth;
